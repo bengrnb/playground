9f4f70c7ea66de9024b9918321b819eb42aa8f93
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-01-22 22:30:34 -0500

    Add smooth scroll to buttons

--- js/scroll.js
+++ js/scroll.js
@@ -6,21 +6,48 @@ window.onscroll = () => {
         document.querySelector("#home-nav").classList.add("active-nav");
         document.querySelector("#about-nav").classList.remove("active-nav");
         document.querySelector("#case-nav").classList.remove("active-nav");
-        console.log("At fold")
     }
     // if you are at the about section and not at the case study section
     else if (scrollY >= sections[0].offsetTop-60 && scrollY < sections[1].offsetTop-60) {
         document.querySelector("#about-nav").classList.add("active-nav");
         document.querySelector("#home-nav").classList.remove("active-nav");
         document.querySelector("#case-nav").classList.remove("active-nav");
-        console.log("At about")
     }
     // if you are past the about section and at the case study section
     else if (scrollY > sections[0].offsetTop-60 && scrollY >= sections[1].offsetTop-60) {
         document.querySelector("#case-nav").classList.add("active-nav");
         document.querySelector("#about-nav").classList.remove("active-nav");
         document.querySelector("#home-nav").classList.remove("active-nav");
-        console.log("At case study")
-
     }
 }
+const home = document.querySelector('#home-nav')
+const about = document.querySelector('#about-nav')
+const caseStudy = document.querySelector('#case-nav')
+const aboutButton = document.querySelector('#about-button')
+const caseButton = document.querySelector('#case-study-button')
+home.addEventListener("click", () => {
+    document.querySelector('#fold').scrollIntoView({
+        behavior: 'smooth'
+      });
+})
+about.addEventListener("click", () => {
+    document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+      });
+})
+caseStudy.addEventListener("click", () => {
+    document.querySelector('#case-studies').scrollIntoView({
+        behavior: 'smooth'
+      });
+})
+aboutButton.addEventListener("click", () => {
+    document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+      });
+})
+caseButton.addEventListener("click", () => {
+    document.querySelector('#case-studies').scrollIntoView({
+        behavior: 'smooth'
+      });
+})
+
