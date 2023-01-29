4673e8abf7b2214a1f3959b7cef32fa16d3041db
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-01-29 11:57:34 -0500

    Change scroll behavior

--- js/scroll.js
+++ js/scroll.js
@@ -3,21 +3,30 @@ const sections = document.querySelectorAll(".section");
 window.onscroll = () => {
     // if you are not at the about section and not at the case study section
     if (scrollY < sections[0].offsetTop-60 && scrollY < sections[1].offsetTop-60) {
-        document.querySelector("#home-nav").classList.add("active-nav");
-        document.querySelector("#about-nav").classList.remove("active-nav");
-        document.querySelector("#case-nav").classList.remove("active-nav");
+        document.querySelector("#home-nav-line").classList.add("nav-line-active");
+        document.querySelector("#home-nav-line").classList.remove("nav-line-inactive");
+        document.querySelector("#about-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#about-nav-line").classList.add("nav-line-inactive");
+        document.querySelector("#case-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#case-nav-line").classList.add("nav-line-inactive");
     }
     // if you are at the about section and not at the case study section
     else if (scrollY >= sections[0].offsetTop-60 && scrollY < sections[1].offsetTop-60) {
-        document.querySelector("#about-nav").classList.add("active-nav");
-        document.querySelector("#home-nav").classList.remove("active-nav");
-        document.querySelector("#case-nav").classList.remove("active-nav");
+        document.querySelector("#about-nav-line").classList.add("nav-line-active");
+        document.querySelector("#about-nav-line").classList.remove("nav-line-inactive");
+        document.querySelector("#home-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#home-nav-line").classList.add("nav-line-inactive");
+        document.querySelector("#case-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#case-nav-line").classList.add("nav-line-inactive");
     }
     // if you are past the about section and at the case study section
     else if (scrollY > sections[0].offsetTop-60 && scrollY >= sections[1].offsetTop-60) {
-        document.querySelector("#case-nav").classList.add("active-nav");
-        document.querySelector("#about-nav").classList.remove("active-nav");
-        document.querySelector("#home-nav").classList.remove("active-nav");
+        document.querySelector("#case-nav-line").classList.add("nav-line-active");
+        document.querySelector("#case-nav-line").classList.remove("nav-line-inactive");
+        document.querySelector("#about-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#about-nav-line").classList.add("nav-line-inactive");
+        document.querySelector("#home-nav-line").classList.remove("nav-line-active");
+        document.querySelector("#home-nav-line").classList.add("nav-line-inactive");
     }
 }
 const home = document.querySelector('#home-nav')
