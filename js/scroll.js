e577f894b07a5eade17658af8d0e168b456f4d55
Ben Greenberg <bengrnb@umich.edu>
2023-01-31 19:32:38 -0500

    Update header active and inactive  text color

--- js/scroll.js
+++ js/scroll.js
@@ -5,28 +5,61 @@ window.onscroll = () => {
     if (scrollY < sections[0].offsetTop-60 && scrollY < sections[1].offsetTop-60) {
         document.querySelector("#home-nav-line").classList.add("nav-line-active");
         document.querySelector("#home-nav-line").classList.remove("nav-line-inactive");
+
+        document.querySelector("#home-nav").classList.add("active-nav");
+        document.querySelector("#home-nav").classList.remove("inactive-nav");
+
         document.querySelector("#about-nav-line").classList.remove("nav-line-active");
         document.querySelector("#about-nav-line").classList.add("nav-line-inactive");
+
+        document.querySelector("#about-nav").classList.remove("active-nav");
+        document.querySelector("#about-nav").classList.add("inactive-nav");
+
         document.querySelector("#case-nav-line").classList.remove("nav-line-active");
         document.querySelector("#case-nav-line").classList.add("nav-line-inactive");
+
+        document.querySelector("#case-nav").classList.remove("active-nav");
+        document.querySelector("#case-nav").classList.add("inactive-nav");
     }
     // if you are at the about section and not at the case study section
     else if (scrollY >= sections[0].offsetTop-60 && scrollY < sections[1].offsetTop-60) {
         document.querySelector("#about-nav-line").classList.add("nav-line-active");
         document.querySelector("#about-nav-line").classList.remove("nav-line-inactive");
+
+        document.querySelector("#about-nav").classList.add("active-nav");
+        document.querySelector("#about-nav").classList.remove("inactive-nav");
+
+        document.querySelector("#home-nav").classList.remove("active-nav");
+        document.querySelector("#home-nav").classList.add("inactive-nav");
+
         document.querySelector("#home-nav-line").classList.remove("nav-line-active");
         document.querySelector("#home-nav-line").classList.add("nav-line-inactive");
+
         document.querySelector("#case-nav-line").classList.remove("nav-line-active");
         document.querySelector("#case-nav-line").classList.add("nav-line-inactive");
+
+        document.querySelector("#case-nav").classList.remove("active-nav");
+        document.querySelector("#case-nav").classList.add("inactive-nav");
     }
     // if you are past the about section and at the case study section
     else if (scrollY > sections[0].offsetTop-60 && scrollY >= sections[1].offsetTop-60) {
         document.querySelector("#case-nav-line").classList.add("nav-line-active");
         document.querySelector("#case-nav-line").classList.remove("nav-line-inactive");
+
+        document.querySelector("#case-nav").classList.add("active-nav");
+        document.querySelector("#case-nav").classList.remove("inactive-nav");
+
         document.querySelector("#about-nav-line").classList.remove("nav-line-active");
         document.querySelector("#about-nav-line").classList.add("nav-line-inactive");
+        
+        document.querySelector("#about-nav").classList.remove("active-nav");
+        document.querySelector("#about-nav").classList.add("inactive-nav");
+        
         document.querySelector("#home-nav-line").classList.remove("nav-line-active");
         document.querySelector("#home-nav-line").classList.add("nav-line-inactive");
+
+        document.querySelector("#home-nav").classList.remove("active-nav");
+        document.querySelector("#home-nav").classList.add("inactive-nav");
     }
 }
 const home = document.querySelector('#home-nav')
