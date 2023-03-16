bd1ade15edd379b67709d8ee452f665d6d5279ed
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-03-16 00:09:11 -0400

    Update footer

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,16 @@
 document.querySelector("footer").innerHTML =
-`<p>Portfolio hand coded by Ben Greenberg</p>
+`<div>
+    <p>Hand coded by Ben Greenberg</p>
+</div>
 <div class="footer-info">
     <a href="./files/BGreenberg-Resume.pdf" target="_blank"><p>Resume</p></a>
     <p>|</p>
     <a href="https://www.linkedin.com/in/bengrnb/" target="_blank"><p>LinkedIn</p></a>
     <p>|</p>
     <p>bengrnb@umich.edu</p>
-</div>
-<div class="footer-info">
+</div>`
+{/* <div class="footer-info">
     <p>Theme</p>
     <button data-set-theme="">Default</button>
     <button data-set-theme="Mocha">Mocha</button>
-</div>`
+</div> */}
