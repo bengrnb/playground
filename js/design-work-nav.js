51c1ec98e35dc7db1efb6c31cced621521207567
bengrnb <66656217+bengrnb@users.noreply.github.com>
2023-03-15 23:52:59 -0400

    Convert header and footer to js for all files

---
+++ js/design-work-nav.js
@@ -0,0 +1,23 @@
+document.querySelector("header").innerHTML =
+`<nav>
+    <div class="nav-container">
+        <div class="nav-items-container">
+            <div class="nav-item">
+                <a href="./" id="home-nav" class="inactive-nav">Home</a>
+                <div id="home-nav-line" class="nav-line-inactive"></div>
+            </div>
+            <div class="nav-item">
+                <a href="./#about" id="about-nav" class="inactive-nav">About</a>
+                <div id="about-nav-line" class="nav-line-inactive"></div>
+            </div>
+            <div class="nav-item">
+                <a href="./#case-studies" id="case-nav" class="inactive-nav">Projects</a>
+                <div id="case-nav-line" class="nav-line-inactive"></div>
+            </div>
+            <div class="nav-item">
+                <a href="./#design-work" id="design-nav" class="active-nav">Design Work</a>
+                <div id="design-nav-line" class="nav-line-active"></div>
+            </div>
+        </div>
+    </div>
+</nav>`
---
+++ js/footer.js
@@ -0,0 +1,14 @@
+document.querySelector("footer").innerHTML =
+`<p>Portfolio hand coded by Ben Greenberg</p>
+<div class="footer-info">
+    <a href="./files/BGreenberg-Resume.pdf" target="_blank"><p>Resume</p></a>
+    <p>|</p>
+    <a href="https://www.linkedin.com/in/bengrnb/" target="_blank"><p>LinkedIn</p></a>
+    <p>|</p>
+    <p>bengrnb@umich.edu</p>
+</div>
+<div class="footer-info">
+    <p>Theme</p>
+    <button data-set-theme="">Default</button>
+    <button data-set-theme="Mocha">Mocha</button>
+</div>`
